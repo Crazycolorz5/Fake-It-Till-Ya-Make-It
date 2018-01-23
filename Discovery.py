0848d025b6d321c9ca57b94c4ede12e715cf3945
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-01-23 16:15:51 -0500

    Added query options and cleaned up.

--- Discovery.py
+++ Discovery.py
@@ -2,18 +2,17 @@ import json
 from watson_developer_cloud import DiscoveryV1
 
 discovery = DiscoveryV1(
-  username="{username}",
-  password="{password}",
+  username="9b933574-a27e-407c-961e-f8ab10d8ad5d",
+  password="IpRnfb244XuP",
   version="2017-11-07"
 )
-
-response = discovery.create_environment(
-  name="my_environment",
-  description="My environment",
-  size=1
-)
+environment_id = "14f0004b-c966-4644-89b7-b5465bcbc116"
+collection_id = "f13d7ea8-a3bb-4f87-bd0f-bba7cb3b94d3"
 
 
-new_collection = discovery.create_collection(response["environment_id"], 'my_collection', description='The main collection for all documents.', configuration_id='{configuration_id}', language='{language}')
+collection = discovery.get_collection(environment_id, collection_id)
+print(json.dumps(collection, indent=2))
 
-print(response) #json.dumps(response, indent=2))
+qopts = {'query': "Wikipedia", "passages" : True}
+my_query = discovery.query(environment_id, collection_id, qopts)
+print(json.dumps(my_query, indent=2))
