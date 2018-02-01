153558a85fc8be5b5379d85344a99f7c2d8fca91
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-02-01 16:22:41 -0500

    Added partial edited documents.

--- Discovery.py
+++ Discovery.py
@@ -1,3 +1,6 @@
+# Usage:
+# python3 Discovery.py "Your query here"
+
 import json
 from watson_developer_cloud import DiscoveryV1
 from sys import argv
@@ -10,11 +13,23 @@ discovery = DiscoveryV1(
 environment_id = "14f0004b-c966-4644-89b7-b5465bcbc116"
 collection_id = "f13d7ea8-a3bb-4f87-bd0f-bba7cb3b94d3"
 
+def take(n, it):
+    i = 0
+    for x in it:
+        if i<n:
+            yield x
+        else:
+            return
+        i += 1
+    
 def askWatson(query, found_documents):
-    qopts = {'natural_language_query': query, 'passages' : True, 'passages.count' : 3, 'count' : 0}
+    qopts = {'natural_language_query': query, 'passages' : True, 'count' : 0} #'passages.count' : 3
     my_query = discovery.query(environment_id, collection_id, qopts)
     results = my_query["passages"]
-    return list(filter(lambda x: x['document_id'] in found_documents, results))
+    return list(map(lambda x: x['passage_text'], 
+                take(3,
+                filter(lambda x: x['document_id'] in found_documents, 
+                       results))))
 
 
 found_documents = set()
