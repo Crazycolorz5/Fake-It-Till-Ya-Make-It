769d37aaed2b45a20338c1dab656aa45871429c3
= <knaplund.2@osu.edu>
2018-03-06 15:22:03 -0500

    Basic testing for GameState

---
+++ test_GameState.py
@@ -0,0 +1,31 @@
+from GameState import *
+from Discovery import *
+
+def test_Player_act():
+    player = PlayerState("Steve Boxwell")
+    '''
+    query = ''
+    result = player.act(query)
+    assert result == "No command specified!"
+    '''
+    query = 'help'
+    result = player.act(query)
+    assert result == player.helpString
+    
+    wat = Watson()
+    query = 'query war of 1812'
+    result = player.act(query)
+    assert result == formatResponse(wat.query(argument))
+    '''
+    query = 'move to the room'
+    result = player.act(query)
+    assert result == 'move to classroom'
+    
+    query = 'move to hallway'
+    result = player.act(query)
+    assert result == 'move to hallway'
+    
+    query = 'walk to student'
+    result = player.act(query)
+    assert result == 'talk to student'
+    '''
