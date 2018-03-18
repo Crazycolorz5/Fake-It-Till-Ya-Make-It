c27f26433229cf591bdd4f66d0f200c0b2a54c98
Dylan Knaplund <Dylan Knaplund>
2018-03-18 16:19:16 -0400

    added tests to test_GameState.py

--- test_GameState.py
+++ test_GameState.py
@@ -12,11 +12,19 @@ def test_Player_act():
     result = player.act(query)
     assert result == player.helpString
     
+    query = 'query'
+    result = player.act(query)
+    assert result == 'No query specified!'
+    
     wat = Watson()
     query = 'query war of 1812'
     watquery = 'war of 1812'
     result = player.act(query)
     assert result == PlayerState.formatResponse(wat.ask(watquery))
+    
+    query = 'question'
+    result = player.act(query)
+    assert result == "Hey Prof., I have a question. What date was the treaty of Ghent signed? (format: MM/DD/YYYY)"
     '''
     query = 'move to the room'
     result = player.act(query)
