7c5858483d6df125f9be23e72d717837a0a2d0ad
mjonnada <35585307+mjonnada@users.noreply.github.com>
2018-03-29 13:46:53 -0400

    Update test_StudentNLC.py
    still testing

--- test_StudentNLC.py
+++ test_StudentNLC.py
@@ -5,8 +5,8 @@ from NLC import *
 
 def test_classify():
  nlc = StudentNLC()
+   
+    query = 'talk to Francis Bacon'
+    result = nlc.classify(query)
+    assert result == 'Francis Bacon'
 
-	query = 'talk Francis Bacon'
-	result = nlc.classify(query)
-	assert result == 'Francis Bacon'	
-	
