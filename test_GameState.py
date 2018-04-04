5ce27ef537539834f2bc1d203888fe9b2096401d
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-04-04 14:30:52 -0400

    Testing all intent before and after talking to all students now.

--- test_GameState.py
+++ test_GameState.py
@@ -5,6 +5,12 @@ def classroomTest(player, classroom):
     for intent in classroom.commandDictionary:
         classroom.commandDictionary[intent](player, classroom)
         player.state = PlayerState.DEFAULT #Don't bother with traversing decision trees.
+    for student in classroom.students:
+        student.talkTo()
+    #Test the intents again in case talking to a student changes state.
+    for intent in classroom.commandDictionary:
+        classroom.commandDictionary[intent](player, classroom)
+        player.state = PlayerState.DEFAULT #Don't bother with traversing decision trees.
 
 def test_GameState():
     player = Player("Steve Boxwell")
