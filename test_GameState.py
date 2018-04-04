5c25fcf7f15e90dc89b499b6870fb1330b8f10f1
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-04-04 14:24:24 -0400

    Fixed order of definitions / helper function name.

--- test_GameState.py
+++ test_GameState.py
@@ -1,13 +1,13 @@
 from GameState import *
 from StateBase import *
+        
+def classroomTest(player, classroom):
+    for intent in classroom.commandDictionary:
+        classroom.commandDictionary[intent](player, classroom)
+        player.state = PlayerState.DEFAULT #Don't bother with traversing decision trees.
 
 def test_GameState():
     player = Player("Steve Boxwell")
     gs = player.gameState
     for classroom in [gs.BiologyClassroom, gs.MathClassroom, gs.LitClassroom, gs.USHistClassroom, gs.WorldHistClassroom, gs.SciencesHallway, gs.ArtsHallway]:
-        testClassroom(player, classroom)
-        
-def testClassroom(player, classroom):
-    for intent in classroom.commandDictionary:
-        classroom.commandDictionary[intent](player, classroom)
-        player.state = PlayerState.DEFAULT #Don't bother with traversing decision trees.
+        classroomTest(player, classroom)
