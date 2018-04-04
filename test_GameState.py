57511d0b3124e1c0225c93e3fb4cef43ad24ac78
Crazycolorz5 <Crazycolorz5@gmail.com>
2018-04-04 14:23:36 -0400

    Added test for each intent of each classroom.

--- test_GameState.py
+++ test_GameState.py
@@ -1,4 +1,13 @@
 from GameState import *
+from StateBase import *
 
 def test_GameState():
-    gs = GameState("Steve Boxwell.")
+    player = Player("Steve Boxwell")
+    gs = player.gameState
+    for classroom in [gs.BiologyClassroom, gs.MathClassroom, gs.LitClassroom, gs.USHistClassroom, gs.WorldHistClassroom, gs.SciencesHallway, gs.ArtsHallway]:
+        testClassroom(player, classroom)
+        
+def testClassroom(player, classroom):
+    for intent in classroom.commandDictionary:
+        classroom.commandDictionary[intent](player, classroom)
+        player.state = PlayerState.DEFAULT #Don't bother with traversing decision trees.
