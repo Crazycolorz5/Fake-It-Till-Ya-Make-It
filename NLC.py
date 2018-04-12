a3a2353737af176f737cfb7c720f737a8383e288
cmason36 <mason.787@osu.edu>
2018-04-12 14:36:32 -0400

    updated classifiers

--- NLC.py
+++ NLC.py
@@ -10,27 +10,31 @@ naturalLanguageClassifier = NaturalLanguageClassifierV1(
 
 #classifier "2fbbc6x326-nlc-1451"
 #created 03/27 from nlc data 03/23
+#classifier '2fbf5cx328-nlc-1775'
+#created 03/29 from nlc data 0329
 
 #stduent classifier '539e6dx331-nlc-551'
 #created 03/27 from student nlc data 03/27
 
 #subject classifier '2fbbc6x326-nlc-1454'
 #created 03/27 from subject nlc data 03/27
+#subject classifier '2fbda2x327-nlc-1810'
+#created 03/29 from subject nlc data 0329
 
 
 #Create Classifier - keep commented
-#with open('subject_nlc_training_set_0329.csv', 'rb') as training_data:
+#with open('nlc_training_set_0412.csv', 'rb') as training_data:
 #    classroomClassifier = naturalLanguageClassifier.create(      
 #        training_data = training_data,
-#        name = 'subject0329'
+#        name = 'nlc0412'
 #        )
 #print(json.dumps(classifiers, indent=2))
 
 classifiers = naturalLanguageClassifier.list()
 #New classifiers
-classifierID =        '2fbf5cx328-nlc-1775'
+classifierID =        'ab2c7bx342-nlc-478'
 studentClassifierID = '2fbf5cx328-nlc-1761'
-subjectClassifierID = '2fbda2x327-nlc-1810'
+subjectClassifierID = 'ab2c7bx342-nlc-477'
 
 CONFIDENCE_THRESHOLD = 0.8
 STUDENT_CONFIDENCE_THRESHOLD = 0.9
@@ -39,7 +43,7 @@ SUBJECT_CONFIDENCE_THRESHOLD = 0.9
 class NLC:
 
     def __init__(self):
-        self.classifierID = '2fbf5cx328-nlc-1775'
+        self.classifierID = 'ab2c7bx342-nlc-478'
         self.status = naturalLanguageClassifier.status(self.classifierID)
         self.subject = SubjectNLC()
         self.student = StudentNLC()
@@ -52,19 +56,19 @@ class NLC:
 
             classes = naturalLanguageClassifier.classify(self.classifierID, string)
             confidence = classes['classes'][0]['confidence']
-            print(classes['top_class'])
-            print(confidence)
+            #print(classes['top_class'])
+            #print(confidence)
             if confidence > CONFIDENCE_THRESHOLD:
                 topClass = classes['top_class']
             #send to StudentNLC/SubjectNLC if needed
             if topClass == 'move to classroom':
-                print('In subject')
+                #print('In subject')
                 tempClass = self.subject.classify(string)
                 if tempClass != 'default' and tempClass != 'cancel':
                     topClass = tempClass
 
             if topClass == 'talk to student':
-                print('In student')
+                #print('In student')
                 tempClass = self.student.classify(string)
                 if tempClass != 'default' and tempClass != 'cancel':
                     topClass = tempClass
@@ -84,15 +88,15 @@ class StudentNLC:
 
             classes = naturalLanguageClassifier.classify(self.studentClassifierID, string)
             confidence = classes['classes'][0]['confidence']
-            print(classes['top_class'])
-            print(confidence)
+            #print(classes['top_class'])
+            #print(confidence)
             if confidence > STUDENT_CONFIDENCE_THRESHOLD:
                 topClass = classes['top_class']
         return topClass
 
 class SubjectNLC:
     def __init__(self):
-        self.subjectClassifierID = '2fbda2x327-nlc-1810'
+        self.subjectClassifierID = 'ab2c7bx342-nlc-477'
         self.status = naturalLanguageClassifier.status(self.subjectClassifierID)
 
     #returns the intent of the classifier given a string
@@ -102,8 +106,8 @@ class SubjectNLC:
         if self.status['status'] == 'Available' and string and not string.isspace():
             classes = naturalLanguageClassifier.classify(self.subjectClassifierID, string)
             confidence = classes['classes'][0]['confidence']
-            print(classes['top_class'])
-            print(confidence)
+            #print(classes['top_class'])
+            #print(confidence)
             if confidence > SUBJECT_CONFIDENCE_THRESHOLD:
                 topClass = classes['top_class']
         return topClass
